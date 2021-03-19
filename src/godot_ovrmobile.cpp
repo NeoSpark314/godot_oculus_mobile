1b4bdf94f41a54d7275742e3e82cc3922017bda2
Holger Dammertz <holger.dammertz@googlemail.com>
2021-03-19 16:32:38 +0100

    renamed api src folder

--- src/godot_ovrmobile.cpp
+++ src/godot_ovrmobile.cpp
@@ -8,11 +8,12 @@
 
 #include "arvr_interface.h"
 
-#include "nativescript_api/ovr_base_api.h"
-#include "nativescript_api/ovr_init_api.h"
-#include "nativescript_api/ovr_utilities.h"
-#include "nativescript_api/ovr_mixed_reality_capture.h"
-#include "nativescript_api/ovr_vr_api_proxy.h"
+#include "ovr_gdscript_api/ovr_base_api.h"
+#include "ovr_gdscript_api/ovr_init_api.h"
+#include "ovr_gdscript_api/ovr_utilities.h"
+#include "ovr_gdscript_api/ovr_compositor_layers.h"
+#include "ovr_gdscript_api/ovr_mixed_reality_capture.h"
+#include "ovr_gdscript_api/ovr_vr_api_proxy.h"
 
 void GDN_EXPORT godot_ovrmobile_gdnative_singleton() {
 	if (arvr_api != NULL) {
@@ -28,6 +29,7 @@ void GDN_EXPORT godot_ovrmobile_nativescript_init(void *p_handle) {
 	register_gdnative_base_api(p_handle);
 	register_gdnative_init_api(p_handle);
 	register_gdnative_utilities(p_handle);
+	register_gdnative_compositor_layers(p_handle);
 	register_gdnative_vr_api_proxy(p_handle);
 	register_gdnative_mrc(p_handle);
 }
